9691f6f35104b8472d791ba579436230f393206e
Adem Mrizak <adem.mrizak@gmail.com>
2020-04-09 20:05:01 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MainComponent } from './main/main.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatTabsModule,
+    MatExpansionModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent implements OnInit {
+
+  apps = [
+    {
+      icon: 'assets/img/gm-icon.png',
+      title: 'Games Master',
+      subtitle: 'Angular',
+      image: 'assets/img/screengm.png',
+      content: 'Application web regroupant plusieurs jeux de sociétés comme le morpion ou encore le puissance 4.',
+      buttonColor: 'accent',
+      link: 'https://adem-m.github.io/games-master/',
+      buttonText: 'Jouer',
+      version: '',
+      os: '',
+      size: '',
+      format: '',
+    },
+    {
+      icon: 'assets/img/mastermind-icon.png',
+      title: 'Mastermind',
+      subtitle: 'C++',
+      image: 'assets/img/screenmastermind.png',
+      content: 'Version numérique du célèbre jeu de plateau. Il est toujours question ici de trouver une combinaison secrète composée de 4 couleurs.En revanche, celle- ci n\'est pas déterminée par un joueur mais générée aléatoirement par mon programme.',
+      buttonColor: 'primary',
+      link: 'assets/mastermind.rar',
+      buttonText: 'Télécharger',
+      version: '1.0',
+      os: 'Windows 7/8/10',
+      size: '9,11 Mo',
+      format: '.rar',
+    },
+    {
+      icon: 'assets/img/pg-icon.png',
+      title: 'Générateur de mot de passe',
+      subtitle: 'C++',
+      image: 'assets/img/screenpassgen.png',
+      content: 'Petit logiciel permettant la génération de mots de passe sécurisés. Plusieurs fonctionnalités y sont disponibles telles que le choix du nombre de caractères et la possibilité d\'enregistrer les mots de passe.',
+      buttonColor: 'primary',
+      link: 'assets/passwordgenerator.rar',
+      buttonText: 'Télécharger',
+      version: '1.0',
+      os: 'Windows 7/8/10',
+      size: '11,0 Mo',
+      format: '.rar',
+    },
+    {
+      icon: 'assets/img/file-icon.png',
+      title: 'File d\'attente',
+      subtitle: 'C++',
+      image: 'assets/img/screenfile.png',
+      content: 'Projet de fin de BTS, ce logiciel est capable d\'afficher et de gérer une file d\'attente pour les requêtes du Robot Charly. Celles-ci sont transmises par Bluetooth via une tablette Android puis exécutées les unes après les autres et stockeés dans une base de données SQLite.',
+      buttonColor: 'primary',
+      link: 'assets/filedattente.rar',
+      buttonText: 'Télécharger',
+      version: '1.0',
+      os: 'Windows 7/8/10',
+      size: '11,7 Mo',
+      format: '.rar',
+    }
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
